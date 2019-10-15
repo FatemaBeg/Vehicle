66c058a29d5ab3f9264b19455c6843803ca7736c
FatemaBeg <fatema007beg@gmail.com>
2019-10-15 19:53:00 +0600

    Vehicle Sppeds Info

---
+++ VechicleAppPractice2/Model/VehicalManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2.Model
+{
+    public class VehicalManager
+    {
+        
+        public string VehicleName { set; get; }
+        public string VehicleRegistrationNo { set; get; }
+    }
+}
---
+++ VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VechicleAppPractice2.Model;
+
+namespace VechicleAppPractice2
+{
+    public partial class Vehicle : Form
+    {
+        VehicalManager vehicle = new VehicalManager();
+
+        List<double> speedsinfo = new List<double>();
+
+        public Vehicle()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            
+            vehicle.VehicleName = vehicleNameTextBox.Text;
+            vehicle.VehicleRegistrationNo = vehicleRegNoTextBox.Text;
+
+         
+            MessageBox.Show("Registration succesfull");
+            vehicleNameTextBox.Text = "";
+            vehicleRegNoTextBox.Text = "";
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(speedTextBox.Text))
+            {
+                speedsinfo.Add(Convert.ToDouble(speedTextBox.Text));
+                    speedTextBox.Text="";
+            }
+            else
+            {
+                MessageBox.Show("Please Enter speed");
+                return;
+            }
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            vehicleNameTextBox.Text = vehicle.VehicleName;
+            vehicleRegNoTextBox.Text = vehicle.VehicleRegistrationNo;
+
+            minSpeedTextBox.Text = speedsinfo.Min().ToString();
+            maxSpeedTextBox.Text = speedsinfo.Max().ToString();
+            averageSpeedTextBox.Text = speedsinfo.Average().ToString();
+        }
+
+        private void speedTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
